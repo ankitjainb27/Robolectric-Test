ad24a4dd7b86de95d96b85ec7d157c77c678e5a5
ankitjainb27 <ankit.jainb27@gmail.com>
2015-08-10 11:32:49 +0530

    added title_correct test and menu inflater test in WelcomeActivityTest

--- app/src/main/java/com/ankit/welcome/WelcomeActivity.java
+++ app/src/main/java/com/ankit/welcome/WelcomeActivity.java
@@ -1,17 +1,21 @@
 package com.ankit.welcome;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Deckard");
+
 
         final View button = findViewById(R.id.login);
         button.setOnClickListener(new View.OnClickListener() {
@@ -20,5 +24,25 @@ public class WelcomeActivity extends Activity {
                 startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                return true;
+
+            case R.id.item2:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/test/java/com/ankit/welcome/WelcomeActivityTest.java
+++ app/src/test/java/com/ankit/welcome/WelcomeActivityTest.java
@@ -1,6 +1,8 @@
 package com.ankit.welcome;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.view.Menu;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +11,7 @@ import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.robolectric.Shadows.shadowOf;
 
 /**
@@ -27,4 +30,20 @@ public class WelcomeActivityTest {
         Intent expectedIntent = new Intent(activity, LoginActivity.class);
         assertThat(shadowOf(activity).getNextStartedActivity()).isEqualTo(expectedIntent);
     }
+
+    @Test
+    public void titleIsCorrect() throws Exception {
+        Activity activity = Robolectric.setupActivity(WelcomeActivity.class);
+        assertTrue(activity.getTitle().toString().equals("Deckard"));
+    }
+
+    @Test
+    public void onCreate_shouldInflateTheMenu() throws Exception {
+        Activity activity = Robolectric.setupActivity(WelcomeActivity.class);
+
+        final Menu menu = shadowOf(activity).getOptionsMenu();
+        assertThat(menu.findItem(R.id.item1).getTitle()).isEqualTo("First menu item");
+        assertThat(menu.findItem(R.id.item2).getTitle()).isEqualTo("Second menu item");
+    }
+
 }
