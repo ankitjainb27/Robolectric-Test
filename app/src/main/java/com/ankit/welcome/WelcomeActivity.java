71ffd0373981184f3de506f39c87d022440405db
ankitjainb27 <ankit.jainb27@gmail.com>
2015-08-10 11:28:39 +0530

    robolectic test on intent running correctly

---
+++ app/src/main/java/com/ankit/welcome/WelcomeActivity.java
@@ -0,0 +1,24 @@
+package com.ankit.welcome;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final View button = findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/test/java/com/ankit/welcome/WelcomeActivityTest.java
@@ -0,0 +1,30 @@
+package com.ankit.welcome;
+
+import android.content.Intent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.robolectric.Shadows.shadowOf;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class WelcomeActivityTest {
+
+    @Test
+    public void clickingLogin_shouldStartLoginActivity() {
+        WelcomeActivity activity = Robolectric.setupActivity(WelcomeActivity.class);
+        activity.findViewById(R.id.login).performClick();
+
+        Intent expectedIntent = new Intent(activity, LoginActivity.class);
+        assertThat(shadowOf(activity).getNextStartedActivity()).isEqualTo(expectedIntent);
+    }
+}
